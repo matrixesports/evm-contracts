39d8c6bac2f79e582ef9fe443ce2ac61de67c25b
rayquaza7 <sarhaangulati737@gmail.com>
2022-08-07 18:18:04 -0700

    tests done

---
+++ test/BattlePassFactory.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "../src/BattlePassFactory.sol";
+import "../src/Crafting.sol";
+import "forge-std/Test.sol";
+
+contract BattlePassFactoryTest is Test {
+    BattlePassFactory factory;
+    Crafting crafting;
+
+    function setUp() public {
+        crafting = new Crafting();
+        factory = new BattlePassFactory(address(crafting));
+    }
+
+    function testConstructor() public {
+        assertEq(address(this), factory.owner());
+        assertEq(address(crafting), factory.craftingProxy());
+    }
+
+    function testDeploy(uint256 creatorId) public {
+        BattlePass bp = factory.deployBattlePass(creatorId);
+        assertEq(
+            address(bp), address(factory.getBattlePassFromUnderlying(creatorId))
+        );
+    }
+
+    function testRevertDeployNonOwner(address hacker) public {
+        vm.assume(hacker != address(this));
+        vm.startPrank(hacker);
+        vm.expectRevert(abi.encodePacked("UNAUTHORIZED"));
+        factory.deployBattlePass(1);
+    }
+
+    function testRevertDeployAlreadyDeployed() public {
+        factory.deployBattlePass(1);
+        vm.expectRevert();
+        factory.deployBattlePass(1);
+    }
+
+    function testIsBattlePassDeployed() public {
+        BattlePass bp;
+        assertFalse(factory.isBattlePassDeployed(bp));
+        bp = factory.deployBattlePass(1);
+        assertTrue(factory.isBattlePassDeployed(bp));
+    }
+}
